5aefa75e72d150c1c0502dde7387a76b0fda94ce
Дмитрий Брезгунов <dmitry.brezgunov@yandex.ru>
2020-04-09 16:51:27 +0300

    Added models

--- posts/models.py
+++ posts/models.py
@@ -7,18 +7,31 @@ User = get_user_model()
 
 
 class Group(models.Model):
-
-    title = models.CharField(max_length = 200)
-    slug = models.SlugField(unique = True)
-    description = models.TextField(max_length = 500)
-    
+    title = models.CharField(max_length=200, verbose_name="Название")
+    slug = models.SlugField(unique=True)
+    description = models.TextField(max_length=500, blank=True, null=True, verbose_name="Описание")
+    class Meta:
+        verbose_name = "Тэг"
+        verbose_name_plural = "Тэги"
     def __str__(self):
-        return ( '%s' % (self.title) )
+        return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    url = models.URLField()
+    LANGUAGE_DICT = [('RU', 'Русский'), ('EN', 'English')]
+    title = models.CharField(max_length=200, verbose_name="Заголовок")
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    url = models.URLField(verbose_name="Ссылка")
     pub_date = models.DateTimeField("Дата публикации", auto_now_add=True, db_index=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="author_posts")
-    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name="group_posts", blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="author_posts",
+                               verbose_name="Пользователь")
+    tags = models.ManyToManyField(Group, on_delete=models.CASCADE,
+                                  related_name="tags_posts", blank=True,
+                                  null=True, verbose_name="Тэги")
+    language = models.CharField(max_length=2, choices=LANGUAGE_DICT, verbose_name="Язык")
+    class Meta:
+        verbose_name = "Публикация"
+        verbose_name_plural = "Публикации"
+    def __str__(self):
+        return self.title
