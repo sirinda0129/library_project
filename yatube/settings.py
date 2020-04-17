718f3894e453c5130e2b90c32e53fe91448d036e
IrinaRostovtseva <ni8003@yandex.ru>
2020-04-17 14:42:48 +0300

    Adds pagination to api and sorts queryset by pub_date in PostViewSet

--- yatube/settings.py
+++ yatube/settings.py
@@ -150,18 +150,19 @@ EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 SITE_ID = 1
 
 CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
-        }
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
 }
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',    ]
+        'rest_framework.authentication.TokenAuthentication', ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 
