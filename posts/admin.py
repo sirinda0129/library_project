aa4880572d42989cfbd8eddb5a7b1cf4cc332cf8
IrinaRostovtseva <ni8003@yandex.ru>
2020-04-24 00:18:01 +0300

    Adds pk field to post and group.

--- posts/admin.py
+++ posts/admin.py
@@ -3,14 +3,14 @@ from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ("title", "description", "url", "pub_date", "approved", "language")
+    list_display = ("pk", "title", "description", "url", "pub_date", "approved", "language")
     search_fields = ("title", "description")
     list_filter = ("approved", "pub_date", "group")
     empty_value_display = "-пусто-"
 
 
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ("title", "slug", "description")
+    list_display = ("pk", "title", "slug", "description")
     search_fields = ("title",)
     prepopulated_fields = {"slug": ("title",)}
 
